c4487e9123c93dd37266caf299842596fb7123c8
157939272@qq.com <157939272@qq.com>
2026-03-09 16:47:25 +0800

    1、暂存 移动端 活动大厅

--- client/rjengineb_Data/StreamingAssets/Bundles/dev/Lua/FGUILayout/Z_Jasin/HuodongPanel.lua
+++ client/rjengineb_Data/StreamingAssets/Bundles/dev/Lua/FGUILayout/Z_Jasin/HuodongPanel.lua
@@ -3,11 +3,13 @@ local HuodongPanel = class("HuodongPanel", BaseFGUILayout)
 
 function HuodongPanel:Create()
 	self._ui = FGUI:ui_delegate(self.component)
-    HuodongPanel.self = self  --绑定组件
+    --HuodongPanel.self = self  --绑定组件
 
-    --FGUI:setOnClickEvent(self._ui.btn_close, function()  --关闭按钮
-    --    FGUI:Close("Z_Jasin", "HuodongPanel")
-    --end)
+    FGUI:SetCloseUIWhenClickOutside(self) --点击空白关闭
+
+    FGUI:setOnClickEvent(self._ui.btn_close, function()  --关闭按钮
+        FGUI:Close("Z_Jasin", "HuodongPanel")
+    end)
 end
 
 return HuodongPanel
