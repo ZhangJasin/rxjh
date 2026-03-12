c47372061291bb3dd2b19d7f96644e661bbeaa08
157939272@qq.com <157939272@qq.com>
2026-03-12 17:56:14 +0800

    1、暂存 活动大厅

--- Mirserver/Mir200/Envir/QuestDiary/Z_Jasin/Changwan.lua
+++ Mirserver/Mir200/Envir/QuestDiary/Z_Jasin/Changwan.lua
@@ -1,9 +1,22 @@
 Changwan = {}
 local filname = "Changwan"
+local itemLst = "香木宝盒#1&百宝箱#1&神匠之盒#1&白银宝盒#1&黄金宝盒#1&碧玉宝盒#1&符文之盒#1&冰灵宝盒#1&宝匣#1"
+
+function Changwan.req(actor)
+    local state = flag(actor, 010)
+    Message.sendmsgEx(actor, "Changwan", "Ret", { param1 = state })
+end
 
 function Changwan.recv(actor)
-    print("服务端领取畅玩特权")
-    sendmsg(actor, 9, "服务端领取畅玩特权")
+    local state = tonumber(flag(actor, 010))
+    if state == 1 then return sendmsg(actor, 9, "请勿重复领取！") end
+    local bagNum = bagnilcount(actor)
+    if bagNum < 9 then return sendmsg(actor, 9, "背包空间不足！") end
+
+    local objInfo = giveitem(actor, itemLst)
+    set(actor, 010, 1)
+    sendmsg(actor, 9, "领取成功！")
+    Message.sendmsgEx(actor, "Changwan", "Ret", { param1 = tonumber(flag(actor, 010)) })
 end
 
 Message.RegisterNetMsg(ssrNetMsgCfg.Changwan, Changwan)
--- client/rjengineb_Data/StreamingAssets/Bundles/dev/Lua/FGUILayout/Z_Jasin/Changwan.lua
+++ client/rjengineb_Data/StreamingAssets/Bundles/dev/Lua/FGUILayout/Z_Jasin/Changwan.lua
@@ -1,11 +1,20 @@
 local BaseFGUILayout = requireFGUI("BaseFGUILayout")
 local Changwan = class("Changwan", BaseFGUILayout)
 local ItemUtil = SL:RequireFile("FGUILayout/Item/ItemUtil")
+Changwan.Instance = nil
+
+function Changwan:Ret(state)
+    if Changwan.Instance then
+        Changwan.Instance:UpdateUI(state)
+    end
+end
 
 function Changwan:Create()
     self._ui = FGUI:ui_delegate(self.component)
+    Changwan.Instance = self
     local isPC = SL:GetValue("IS_PC_OPER_MODE")
     FGUI:SetCloseUIWhenClickOutside(self)
+
     --关闭按钮
     FGUI:setOnClickEvent(self._ui.btn_close, function()
         FGUI:Close("Z_Jasin", isPC and "PCChangwan" or "Changwan")
@@ -18,6 +27,11 @@ function Changwan:Create()
         FGUI:Close("Z_Jasin", isPC and "PCChangwan" or "Changwan")
     end)
 
+    ssrMessage:sendmsgEx("Changwan", "req")
+end
+
+function Changwan:UpdateUI(state)
+    local isPC = SL:GetValue("IS_PC_OPER_MODE")
     --加载item
     local itemLst = {
         [1] = 1319,
@@ -31,33 +45,49 @@ function Changwan:Create()
         [9] = 1327,
     }
     for i, v in ipairs(itemLst) do
-        local item = FGUI:GetChild(self._ui["item" .. i], "commonItem")
-        if item then
-            ItemUtil:RefreshItemUIByData(item, SL:GetValue("ITEM_DATA", v))
-            FGUI:setOnRollOverEvent(item, function()
-                local tipData = {}
-                tipData.itemData = SL:GetValue("ITEM_DATA", v)
-                tipData.hideCompare = true
-                tipData.hideButtons = true
-                FGUIFunction:OpenItemTips(tipData)
-            end)
-            FGUI:setOnRollOutEvent(item, function()
-                FGUIFunction:CloseItemTips()
-            end)
+        local childNode = self._ui["item" .. i]
+        if childNode then
+            local item = FGUI:GetChild(childNode, "commonItem")
+            if item then
+                ItemUtil:RefreshItemUIByData(item, SL:GetValue("ITEM_DATA", v))
+                if isPC then
+                    FGUI:setOnRollOverEvent(item, function()
+                        local tipData = {}
+                        tipData.itemData = SL:GetValue("ITEM_DATA", v)
+                        tipData.hideCompare = true
+                        tipData.hideButtons = true
+                        FGUIFunction:OpenItemTips(tipData)
+                    end)
+                    FGUI:setOnRollOutEvent(item, function()
+                        FGUIFunction:CloseItemTips()
+                    end)
+                else
+                    FGUI:setOnClickEvent(item, function()
+                        local tipData = {}
+                        tipData.itemData = SL:GetValue("ITEM_DATA", v)
+                        tipData.hideCompare = true
+                        tipData.hideButtons = true
+                        FGUIFunction:OpenItemTips(tipData)
+                    end)
+                end
+            end
         end
     end
 
+    FGUI:GButton_setTitle(self._ui.btn_sub, state.param1 == 0 and "领 取" or "已 领 取")
+
     --领取按钮
     FGUI:setOnClickEvent(self._ui.btn_sub, function()
-        SL:dump("领取")
         ssrMessage:sendmsgEx("Changwan", "recv")
     end)
 end
 
 function Changwan:Destroy()
+    Changwan.Instance = nil
 end
 
 function Changwan:Exit()
 end
 
+ssrMessage:RegisterNetMsg(ssrNetMsgCfg.Changwan, Changwan)
 return Changwan
