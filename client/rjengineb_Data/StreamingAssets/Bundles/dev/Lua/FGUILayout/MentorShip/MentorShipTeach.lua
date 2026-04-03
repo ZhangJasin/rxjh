9044d828decfa1747e76a3bd6bcc46a494f23bb9
PC-20240407YJLT\Administrator <479218183@qq.com>
2026-04-03 11:55:00 +0800

    属性显示去掉小数点

--- client/rjengineb_Data/StreamingAssets/Bundles/dev/Lua/FGUILayout/MentorShip/MentorShipTeach.lua
+++ client/rjengineb_Data/StreamingAssets/Bundles/dev/Lua/FGUILayout/MentorShip/MentorShipTeach.lua
@@ -609,7 +609,7 @@ function MentorShipTeach:RefreshView()
 				data.Desc = cfg.Desc
 				data.ShowValue = data.maxValue
 				if cfg.Type == 1 then
-					data.ShowValue = data.maxValue * 0.01 .. "%"
+					data.ShowValue = string.format("%.0f%%", data.maxValue / 100)
 				elseif cfg.Type == 0 then
 				else
 					SL:PrintEx("未知属性ID = "..v.id.."属性Type" .. cfg.Type)
