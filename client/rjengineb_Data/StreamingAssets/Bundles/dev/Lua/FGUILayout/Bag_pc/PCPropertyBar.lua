f83c491882fe3665b51605acc62a220810983a9b
PC-20240407YJLT\Administrator <479218183@qq.com>
2026-04-03 15:22:08 +0800

    补提

--- client/rjengineb_Data/StreamingAssets/Bundles/dev/Lua/FGUILayout/Bag_pc/PCPropertyBar.lua
+++ client/rjengineb_Data/StreamingAssets/Bundles/dev/Lua/FGUILayout/Bag_pc/PCPropertyBar.lua
@@ -106,6 +106,7 @@ end
 
 function PCPropertyBar:AttrItemRender(idx, item)
     local attrData = self.attrDymicLoadTable[idx+1]
+    local value = attrData.value
     -- Type==1为百分比类型，去掉小数点（如 "0.0%" -> "0%"）
     if attrData.data.Type == 1 and value then
         value = string.match(value, "^([0-9]+)%.?.*")
