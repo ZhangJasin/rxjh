1128a1532787c7a560d464ec6b5f92d6ec4da9d7
PC-20241223KGUW\Administrator <1194605869@qq.com>
2026-05-09 16:55:44 +0800

    添加活跃宝箱，调整BOSS狩猎tips

--- client/rjengineb_Data/StreamingAssets/Bundles/dev/Lua/game_config/cfgcsv/TipsDetail.lua
+++ client/rjengineb_Data/StreamingAssets/Bundles/dev/Lua/game_config/cfgcsv/TipsDetail.lua
@@ -7,7 +7,7 @@ local config = {
 	[2] = { 
 		ID=2,
 		title="BOSS悬赏",
-		tips="1.每日可免费挑战2次，2次过后每次挑战将扣减一个道具“悬赏令”",
+		tips="1.每日可免费挑战 <font color=\"#ff0000\">2 次</font>，开通赞助可额外免费挑战<font color=\"#ff0000\">2 次</font>。<br>2.免费次数使用完后，可通过<font color=\"#ff0000\">悬赏令</font>直接挑战 BOSS。<br>3.每个 BOSS 最多每天挑战<font color=\"#ff0000\">5 次</font>，每天挑战 BOSS 总上限为<font color=\"#ff0000\">20 次</font>。<br>4.可通过<font color=\"#ff0000\">刷新卷</font>刷新 BOSS。<br>5.每个 BOSS 都有专属掉落，可概率掉落对应的专属道具，并且狩猎 BOSS 额外掉落<font color=\"#ff0000\">2 个神匠之盒</font>。<br>",
 	},
 }
 return config
