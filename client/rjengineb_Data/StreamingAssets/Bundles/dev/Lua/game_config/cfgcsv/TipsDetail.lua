6d3f384499bd7fdfeb762ccfa3b56ab19d98dfdb
PC-20240407YJLT\Administrator <479218183@qq.com>
2026-05-11 17:43:25 +0800

    tips占位

--- client/rjengineb_Data/StreamingAssets/Bundles/dev/Lua/game_config/cfgcsv/TipsDetail.lua
+++ client/rjengineb_Data/StreamingAssets/Bundles/dev/Lua/game_config/cfgcsv/TipsDetail.lua
@@ -9,5 +9,10 @@ local config = {
 		title="BOSS悬赏",
 		tips="1.每日可免费挑战 <font color=\"#ff0000\">2 次</font>，开通赞助可额外免费挑战<font color=\"#ff0000\">2 次</font>。<br>2.免费次数使用完后，可通过<font color=\"#ff0000\">悬赏令</font>直接挑战 BOSS。<br>3.每个 BOSS 最多每天挑战<font color=\"#ff0000\">5 次</font>，每天挑战 BOSS 总上限为<font color=\"#ff0000\">20 次</font>。<br>4.可通过<font color=\"#ff0000\">刷新卷</font>刷新 BOSS。<br>5.每个 BOSS 都有专属掉落，可概率掉落对应的专属道具，并且狩猎 BOSS 额外掉落<font color=\"#ff0000\">2 个神匠之盒</font>。<br>",
 	},
+	[3] = { 
+		ID=3,
+		title="江湖历练",
+		tips="1.活跃任务无需接取，在此界面中展示，每到达要求，自动完成",
+	},
 }
 return config
