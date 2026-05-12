32e8dd970333b268bb66d9c478b5f04f70a5caff
157939272@qq.com <157939272@qq.com>
2026-05-12 21:46:52 +0800

    1、修复师徒查看人物

--- client/rjengineb_Data/StreamingAssets/Bundles/dev/Lua/FGUILayout/FuncDock/FuncDock.lua
+++ client/rjengineb_Data/StreamingAssets/Bundles/dev/Lua/FGUILayout/FuncDock/FuncDock.lua
@@ -16,22 +16,27 @@ function FuncDock.main()
         FGUIFunction:LookRankPlayerInfo(FuncDock.openData)
     end)
 
-    SL:RegisterLUAEvent(LUA_EVENT_RESPONSE_LOOK_PLAYER_INFO, "FuncDock", function()
-        --if not SL:GetValue("IS_PC_OPER_MODE") then
-        --    --查看 0:单装备面板 1:装备+属性面板
-        --    local value = SL:GetValue("GAME_DATA", "lookPlayerMode")
-        --    if not value then
-        --        --FGUI:Open("Bag", "LookPlayerPanel", 1)
-        --    else
-        --        if value == 1 then
-        --            -- FGUI:Open("Bag", "LookPlayerPanel", 1)
-        --        else
-        --            --FGUI:Open("Bag", "LookPlayerSingleEquipPanel")
-        --        end
-        --    end
-        --else
-        --    --FGUI:Open("Bag_pc", "PCLookPlayerPanel", 0)
-        --end
+    SL:RegisterLUAEvent(LUA_EVENT_RESPONSE_LOOK_PLAYER_INFO, "FuncDock", function(operateType)
+        -- operateType 为 666 时是师徒发布界面在获取自身模型信息，不打开面板
+        if operateType == 666 then
+            return
+        end
+
+        if not SL:GetValue("IS_PC_OPER_MODE") then
+            --查看 0:单装备面板 1:装备+属性面板
+            local value = SL:GetValue("GAME_DATA", "lookPlayerMode")
+            if not value then
+                FGUI:Open("Bag", "LookPlayerPanel", 1)
+            else
+                if value == 1 then
+                    FGUI:Open("Bag", "LookPlayerPanel", 1)
+                else
+                    FGUI:Open("Bag", "LookPlayerSingleEquipPanel")
+                end
+            end
+        else
+            FGUI:Open("Bag_pc", "PCLookPlayerPanel", 0)
+        end
     end)
 end
 
